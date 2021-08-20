c5487b5074c4ac2d5e5b67190db2fef489f53c1a
Greg Sleap <greg.sleap@curtin.edu.au>
2021-08-20 11:52:04 +0800

    Removed constants SPEED_OF_LIGHT_IN_VACUUM_M_PER_S, MWA_LATITUDE_RADIANS, MWA_LONGITUDE_RADIANS, MWA_ALTITUDE_METRES as they are provided by mwa_rust_code repo

--- src/lib.rs
+++ src/lib.rs
@@ -22,16 +22,8 @@ mod timestep;
 mod voltage_context;
 mod voltage_files;
 
-/// The MWA's latitude on Earth in radians. This is -26d42m11.94986s.
-pub const MWA_LATITUDE_RADIANS: f64 = -0.4660608448386394;
-/// The MWA's longitude on Earth in radians. This is 116d40m14.93485s.
-pub const MWA_LONGITUDE_RADIANS: f64 = 2.0362898668561042;
-/// The MWA's altitude in metres.
-pub const MWA_ALTITUDE_METRES: f64 = 377.827;
 /// the velocity factor of electic fields in RG-6 like coax cable
 pub const MWA_COAX_V_FACTOR: f64 = 1.204;
-/// speed of light in m/s
-pub const SPEED_OF_LIGHT_IN_VACUUM_M_PER_S: f64 = 299792458.0;
 /// the number of seconds per file in MWA Legacy Recmbined VCS
 pub(crate) const MWA_VCS_LEGACY_RECOMBINED_FILE_SECONDS: u64 = 1;
 /// the number of seconds per subfile in MWAX v2 VCS
