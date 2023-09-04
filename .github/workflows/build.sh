eb130c8d47041f5d71ad27892269661764d43f39
Greg Sleap <greg.sleap@curtin.edu.au>
2023-09-04 17:15:33 +1000

    More CI fixes

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -3,6 +3,23 @@
 set -eux
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    #
+    # Build cfitsio
+    #
+    curl "https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-3.49.tar.gz" -o cfitsio.tar.gz
+    tar -xf cfitsio.tar.gz
+    rm cfitsio.tar.gz
+    cd cfitsio-3.49
+    # Enabling SSE2/SSSE3 could cause portability problems, but it's unlikely that anyone
+    # is using such a CPU...
+    # https://stackoverflow.com/questions/52858556/most-recent-processor-without-support-of-ssse3-instructions
+    # Disabling curl just means you cannot fits_open() using a URL.
+    CFLAGS="-O3" ./configure --prefix=/usr/local --enable-reentrant --enable-sse2 --enable-ssse3 --disable-curl
+    make -j
+    sudo make install
+    sudo ldconfig
+    cd ..
+    
     PATH=/root/.cargo/bin:$PATH
     # 1.63 is the newest rustc version that can use glibc >= 2.11, and we use it
     # because newer versions require glibc >= 2.17 (which this container
@@ -10,6 +27,8 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     # glibc).
     rustup install 1.64 --no-self-update
     rustup default 1.64
+
+    # Setup maturin
     pip3 install maturin==1.2.3
 
     # Build a release for each x86_64 microarchitecture level. v4 can't be
@@ -33,6 +52,23 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
             ./*.whl
     done
 elif [[ "$OSTYPE" == "darwin"* ]]; then
+    #
+    # Build cfitsio      
+    #
+    curl "https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-3.49.tar.gz" -o cfitsio.tar.gz
+    tar -xf cfitsio.tar.gz
+    rm cfitsio.tar.gz
+    cd cfitsio-3.49
+    # Enabling SSE2/SSSE3 could cause portability problems, but it's unlikely that anyone
+    # is using such a CPU...
+    # https://stackoverflow.com/questions/52858556/most-recent-processor-without-support-of-ssse3-instructions
+    # Disabling curl just means you cannot fits_open() using a URL.
+    CFLAGS="-O3" ./configure --prefix=/usr/local --enable-reentrant --enable-sse2 --enable-ssse3 --disable-curl
+    sudo make shared
+    sudo make install
+    cd ..
+
+    # Setup maturin
     pip3 install maturin==1.2.3
     brew install automake
 
